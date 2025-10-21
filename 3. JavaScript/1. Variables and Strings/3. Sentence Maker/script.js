ce5c35a01179285f83fa395bfacef2d584290efe
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-10-21 17:06:53 +0200

    fin project

---
+++ 3. JavaScript/1. Variables and Strings/3. Sentence Maker/script.js
@@ -0,0 +1,28 @@
+let adjective
+let noun
+let verb
+let place
+let adjective2
+let noun2
+
+adjective = 'cool'
+noun = 'orange'
+verb = 'read'
+place = 'table'
+adjective2 = 'great'
+noun2 = 'book'
+
+let firstStory
+firstStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`
+console.log(`First story: ${firstStory}`)
+
+adjective = 'red'
+noun = 'mouse'
+verb = 'learn'
+place = 'house'
+adjective2 = 'long'
+noun2 = 'wolf'
+
+let secondStory
+secondStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`
+console.log(`Second story: ${secondStory}`)
