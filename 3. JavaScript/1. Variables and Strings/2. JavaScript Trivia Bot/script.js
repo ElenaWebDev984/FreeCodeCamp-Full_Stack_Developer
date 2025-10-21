1e4be55a09738841aba56b923e310ce60df9629a
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-10-21 16:33:59 +0200

    fin project

---
+++ 3. JavaScript/1. Variables and Strings/2. JavaScript Trivia Bot/script.js
@@ -0,0 +1,13 @@
+console.log("Hello! I'm your coding fun fact guide!")
+const botName = "Bot";
+const botLocation = 'Barcelona'
+const favoriteLanguage = 'JavaScript'
+console.log(`My name is ${botName} and I live on ${botLocation}.`);
+console.log(`My favorite programming language is ${favoriteLanguage}.`);
+let codingFact = `My ${favoriteLanguage} is JS`;
+console.log(codingFact)
+codingFact = `${favoriteLanguage} is a cool language.`
+console.log(codingFact)
+codingFact = `${favoriteLanguage} is  very easy.`
+console.log(codingFact)
+console.log(`It was fun sharing these facts with you. Goodbye! - ${botName} from ${botLocation}.`)
